ca9d1c44017211e20004e11747afbbbdb7905e64
TEJASTATODE <tejastatode449@gmail.com>
2026-01-17 21:32:21 +0530

    finals

--- MLbackend/main.py
+++ MLbackend/main.py
@@ -18,7 +18,7 @@ def read_root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://medi-lens-pi.vercel.app","https://medilens-1.onrender.com"],
+    allow_origins=["http://localhost:5173", "https://medi-lens-pi.vercel.app","https://d2mjyalgrqdo8v.cloudfront.net"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
